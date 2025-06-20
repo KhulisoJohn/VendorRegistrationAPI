75be7e7a81b44f4cd477d483def97a2d8bcedc99
KhulisoJohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-06-20 10:57:13 +0200

    initial commit

--- obj/Debug/net8.0/VendorRegistration.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/VendorRegistration.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VendorRegistration.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49943a66fbf19aeb77de43623dc7ecc4d1ae8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e045dda67bf45a9ef9b8ca7b4940b856cedc0c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("VendorRegistration.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VendorRegistration.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
